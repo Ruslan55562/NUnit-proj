4635df8d3f37c367e01bc0cd625c96b5235de66b
Ruslan <email123@mail.ru>
2021-08-10 16:04:24 +0300

    NUnit commit

---
+++ TestProject.Tests/TestProject.Tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject.Tests
+{
+    [TestFixture]
+    public class TestProjectTests
+    {
+
+
+        [Test]
+        public void Test1()
+        {
+            test("Hello", "Hello");
+            test("123", "123");
+
+            void test(string first, string second)
+            {
+                Assert.AreEqual(first, second, "The strings aren't equal");
+            }
+        }
+        [Test]
+        public void Test2()
+        {
+            string[] first = { "Hello", "Hi" };
+            string[] second = { "Hello", "Hi" };
+            test(first, second);
+
+
+            void test(string[] first, string[] second)
+            {
+                bool ActualResult = TestProjectClass.StringArr(first, second);
+                Assert.AreEqual(true, ActualResult, "The arrays aren't equal");
+            }
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string[] first = { "Hello", "Hi", "How", "Are", "You" };
+            string second = "How";
+            test(first, second);
+
+
+            void test(string[] first, string second)
+            {
+                bool actualResult = TestProjectClass.StringIsInArr(first, second);
+                Assert.AreEqual(true, actualResult, "There is no such string in array");
+            }
+        }
+
+
+        [Test]
+        public void Test4()
+        {
+            test(10, 5);
+            test(9, 8);
+            test(122, 0);
+            test(-4, -5);
+           
+
+
+            void test(int first, int second)
+            {
+                bool actualResult = TestProjectClass.MoreThan(first, second);
+                Assert.AreEqual(true, actualResult, "The first number is less than second");
+            }
+        }
+    }
+    }
+
---
+++ TestProject/TestProject/TestProjectClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class TestProjectClass
+    {
+        static void Main()
+        {
+           
+        }
+        public static bool StringEqual(string p_first, string p_second)
+        {
+            return p_first == p_second;
+        }
+        public static bool StringArr(string [] f_arr,string [] s_arr)
+        {
+            for(int i=0;i<f_arr.Length;i++)
+            { 
+                if(f_arr[i] != s_arr[i])  { return false; }
+            }
+            return true;
+        }
+        public static bool StringIsInArr(string[] p_arr, string p_second)
+        {
+            for(int i=0;i<p_arr.Length;i++)
+            {
+                if(p_arr[i]==p_second){ return true; }
+            }
+            return false;
+        }
+        public static bool MoreThan(int a,int b) { return a > b; }
+    }
+}
