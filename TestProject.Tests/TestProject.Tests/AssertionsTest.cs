176816ca35ec889229610162a794603f576c7b67
Ruslan <email123@mail.ru>
2021-08-11 10:40:51 +0300

    Change Tests(Realization)

--- TestProject.Tests/TestProject.Tests/AssertionsTest.cs
+++ TestProject.Tests/TestProject.Tests/AssertionsTest.cs
@@ -24,30 +24,31 @@ namespace TestProject.Tests
             }
         }
         [Test]
-        public void EqualArrays()
+        public void EqualLists()
         {
-            string[] first = { "Hello", "Hi" };
-            string[] second = { "Hello", "Hi" };
+            List<string>first = new List<string> { "Hello", "Hi" };
+            List<string>second = new List<string> { "Hello", "Hi" };
             test(first, second);
 
-
-            static void test(string[] firstArr, string[] secondArr)
+            static void test(List<string> firstLst, List<string> secondLst)
             {
-                Assert.AreEqual(true, TestProjectClass.StringArr(firstArr, secondArr), "The arrays aren't equal");
+                for (int i = 0; i < firstLst.Count; i++)
+                {
+                    Assert.AreEqual(firstLst[i], secondLst[i], "The lists aren't equal");
+                }
             }
         }
 
         [Test]
         public void Word_In_Array()
         {
-            string[] first = { "Hello", "Hi", "How", "Are", "You" };
+            List<string> firstLst = new List <string>{ "Hello", "Hi", "How", "Are", "You" };
             string second = "How";
-            test(first, second);
+            test(firstLst, second);
 
-
-            static void test(string[] StrArr, string P_second)
+            static void test(List<string> StrLst, string P_second)
             {
-                Assert.AreEqual(true, TestProjectClass.StringIsInArr(StrArr, P_second), "There is no such word in array");
+                Assert.Contains(P_second, StrLst, "There is no such word in array");
             }
         }
 
@@ -55,15 +56,25 @@ namespace TestProject.Tests
         [Test]
         public void Compare_two_numbers()
         {
-            test(10, 5);
+            test(6, 5);
             test(9, 8);
             test(122, 0);
             test(-4, -5);
 
+            //For Assert.IsFalse
+            //test(2, 3);
+            //test(0, 1);
+            //test(-6, -5);
+
+
 
             static void test(int FirstNumb, int SecondNumb)
             {
-                Assert.AreEqual(true, TestProjectClass.MoreThan(FirstNumb, SecondNumb), "The first number is less than second");             
+                // Assert.AreEqual(true, TestProjectClass.MoreThan(FirstNumb, SecondNumb), "The first number is less than second");
+                 Assert.Greater(FirstNumb, SecondNumb, "The first number is less than second");
+                //Assert.IsTrue(TestProjectClass.MoreThan(FirstNumb, SecondNumb), "The first number is less than second");
+                //Assert.IsTrue(FirstNumb > SecondNumb);
+                //Assert.IsFalse(TestProjectClass.MoreThan(FirstNumb, SecondNumb), "The first number is more than second");
             }
         }
 
